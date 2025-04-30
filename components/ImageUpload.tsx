741a3b479778c64436123632b9adf7369757a0b0
Iván Manzaba <mauricio.9.inm@gmail.com>
2025-04-30 14:21:46 -0500

    refactor: update imports in ImageUpload component and clean up pnpm-lock.yaml

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,6 +1,6 @@
 'use client'
-
-import { Suspense, useCallback, useEffect, useState } from 'react'
+import React from 'react'
+import { useCallback, useEffect} from 'react'
 import { X, UploadCloud } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import { SkeletonGrid } from './skeletons/SkeletonGridImages'
